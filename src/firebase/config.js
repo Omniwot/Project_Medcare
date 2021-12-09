ae90b5594e8a0a1fe55f0a0323191b4220b86bdd
Omniwot <52799841+Omniwot@users.noreply.github.com>
2021-12-10 04:45:03 +0530

    Delete src/firebase directory

--- src/firebase/config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyBa5x3qbfFmJdLV5-c3gEfjgZ8tIQtYvvs",
-  authDomain: "medpedia-f0b62.firebaseapp.com",
-  projectId: "medpedia-f0b62",
-  storageBucket: "medpedia-f0b62.appspot.com",
-  messagingSenderId: "23018761444",
-  appId: "1:23018761444:web:c5c025f2e2d629bb4990f1",
-  measurementId: "G-4RNNQ40GV0"
-};
