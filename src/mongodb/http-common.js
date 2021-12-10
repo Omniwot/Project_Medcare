a545313420649a83cd58e4fec7e79b1a5301d622
Omniwot <52799841+Omniwot@users.noreply.github.com>
2021-12-10 06:04:16 +0530

    Delete src/mongodb directory

--- src/mongodb/http-common.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-  baseURL: "https://webhooks.mongodb-realm.com/api/client/v2.0/app/pricesearch-pwrns/service/prices/incoming_webhook/",
-  headers: {
-    "Content-type": "application/json"
-  }
-});
